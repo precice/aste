b7b35c0016abb87e0bcac3c85b749db07f663269
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-02-22 10:18:26 +0100

    Cleaned up timings.py

--- RBF_Timings/timings.py
+++ RBF_Timings/timings.py
@@ -3,20 +3,13 @@ import contextlib, datetime, multiprocessing, os, time, subprocess, sys, socket,
 from pathlib import Path
 
 import numpy as np
-# import pandas
-# import matplotlib.pyplot as plt
-# import matplotlib.ticker as ticker
-# # from plot_helper import set_save_fig_params
 
 try:
     from ipdb import set_trace
 except ImportError:
     from pdb import set_trace
 
-sys.path.append(os.environ['PRECICE_ROOT'] + "/tools")
-import EventTimings
 
-# colors = [ i['color'] for i in plt.rcParams['axes.prop_cycle'] ]
 
 def removeEventFiles(participant):
     p = Path(participant)
@@ -28,6 +21,7 @@ def removeEventFiles(participant):
         
 
 def shape_parameter(mesh_size, m):
+    """ Computes the shape parameter for Gaussians for a given m and mesh size. """
     h = 1 / mesh_size
     s = np.sqrt(- np.log(1e-9)) / (m * h)
     print("################## mesh_size =", mesh_size, ', m =', m, ", h =", h, ", s =", s, "##################")
@@ -58,87 +52,11 @@ def prepareConfigTemplate(shape_parameter, preallocation):
         template = f.read()
     with open("precice.xml", "w") as f:
         f.write(template.format(shape_parameter = shape_parameter, preallocation = preallocation))
+
         
 def createMesh(size):
     cmd = "../make_mesh.py {0} {0}".format(size)
     subprocess.run(cmd, shell = True, check = True)
-
-
-def save_plot(filename, **kwargs):
-    fn = filename.format(**kwargs)
-    plt.savefig(fn + '.pdf')
-    plt.savefig(fn + '.pgf')
-
-def measureRanks(ranks, prealloc, mesh_size_func, m):
-    data = pandas.DataFrame()
-    for rank in ranks:
-        mesh_size = int(mesh_size_func(rank))
-        createMesh(mesh_size)
-        prepareConfigTemplate(shape_parameter(mesh_size, m), prealloc)
-        launchRun(rank)
-        timings = getLatestTimings()['avg']
-        timings.name = rank
-        data = data.append(timings)
-
-    return data
-
-
-def measureUni(ranks, sizes, prealloc, m):
-    data = pandas.DataFrame()
-    for rank, size in zip(ranks, sizes):
-        createMesh(size)
-        prepareConfigTemplate(shape_parameter(size, m), prealloc)
-        launchRun(rank)
-        timings = getLatestTimings()['avg']
-        timings.name = rank
-        data = data.append(timings)
-
-    return data
-
-    
-
-def measureScaling(mesh_size_func, filename):
-    ms = [4, 6, 8]
-    
-    ranks = [2, 3, 4, 6, 8, 10, 14, 18, 24, 28, 32, 36]
-    # ranks = [2, 3, 4, 5]
-
-    set_save_fig_params(rows = 3)
-    
-    # Use preallocation
-    fields = ['PetRBF.fillC', 'PetRBF.fillA', 'PetRBF.PostFill',
-              'map', 'PetRBF.PreallocC', 'PetRBF.PreallocA']
-    fig, ax = plt.subplots(len(ms), sharex = True)
-    for i, m in enumerate(ms):
-        data = measureRanks(ranks, True, mesh_size_func, m)
-        # set_trace()
-        # data = data.reindex(columns = fields)
-        data[fields].plot(ax = ax[i], logy = True, legend = False, style = '-d', sharex = True)
-        ax[i].xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-        ax[i].set_ylabel("Time [ms]")
-        ax[i].set_title('m = {}'.format(m))
-
-    
-    ax[0].legend()
-    ax[-1].set_xlabel("Processors")
-    save_plot(filename, prealloc = "prealloc")
-    plt.close()
-
-    # Do NOT use preallocation
-    fields = ['PetRBF.fillC', 'PetRBF.fillA', 'PetRBF.PostFill', 'map']
-    fig, ax = plt.subplots(len(ms), sharex = True)
-    for i, m in enumerate(ms):
-        data = measureRanks(ranks, False, mesh_size_func, m)
-        # data = data.reindex(columns = fields)
-        data[fields].plot(ax = ax[i], logy = True, legend = False, style = '-d', sharex = True)
-        ax[i].xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-        ax[i].set_ylabel("Time [ms]")
-        ax[i].set_title('m = {}'.format(m))
-
-    ax[0].legend()
-    ax[-1].set_xlabel("Processors")
-    save_plot(filename, prealloc = "noprealloc")
-    plt.close()
 
 
 def doScaling(name, ranksA, ranksB, mesh_sizes, ms, preallocations):
@@ -175,100 +93,30 @@ def doScaling(name, ranksA, ranksB, mesh_sizes, ms, preallocations):
         json.dump({"name"  : name, "date" : file_info["date"], "host" : socket.getfqdn(),
                    "ranksA" : ranksA, "ranksB" : ranksB,
                    "mesh_sizes" : mesh_sizes, "m" : m,
-                   "preallocations" : preallocations},
+                   "preallocation" : preallocations},
                   f)
                 
-    
 
-def comparePreAllocVsNon(mesh_size_func, filename):
-    ranks = [4, 6, 8, 10, 14, 18, 24, 28, 32, 36, 40]
-    # ranks = [2,3,4]
-
-    ms = [4, 6, 8]
-
-    set_save_fig_params(rows = 3)
-    
-    fig, ax = plt.subplots(len(ms), sharex = True)
-    for i, m in enumerate(ms):
-        data = measureRanks(ranks, 'compute', mesh_size_func, m)
-        data.plot(ax = ax[i], y = 'computeMapping', logy = True, legend = False, style = '-d',
-                          sharex = True, label = "compute preallocation")
-        
-        data = measureRanks(ranks, 'mark', mesh_size_func, m)
-        data.plot(ax = ax[i], y = 'computeMapping', logy = False, legend = False, style = '-d',
-                          sharex = True, label = "saved preallocation")
-        
-        data = measureRanks(ranks, 'off', mesh_size_func, m)
-        data.plot(ax = ax[i], y = 'computeMapping', logy = False, legend = False, style = '-d',
-                          sharex = True, label = "without preallocation")
-
-        data = measureRanks(ranks, 'tree', mesh_size_func, m)
-        data.plot(ax = ax[i], y = 'computeMapping', logy = False, legend = False, style = '-d',
-                          sharex = True, label = "tree preallocation")
-        
-        
-        ax[i].xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-        ax[i].set_ylabel("Time [ms]")
-        ax[i].set_title('m = {}'.format(m))
-    
-    ax[0].legend()
-    ax[-1].set_xlabel("Processors")
-    save_plot(filename)
-    plt.close()
-
-
-def problemUpscaling(filename):
-    mesh_sizes = [10, 20, 50, 75, 125, 150, 175, 200, 225, 250, 275, 300]
-    ranks = [8] * len(mesh_sizes)
-    ms = [4, 6, 8]    
-
-    set_save_fig_params(rows = 3)
-    
-    fig, ax = plt.subplots(len(ms), sharex = True)
-    for i, m in enumerate(ms):
-        data = measureUni(ranks, mesh_sizes, 'compute', m)
-        ax[i].loglog(mesh_sizes, data["computeMapping"].values, "-d", label = "computed")
-                
-        data = measureUni(ranks, mesh_sizes, 'mark', m)
-        ax[i].loglog(mesh_sizes, data["computeMapping"].values, "-d", label = "saved")
-        
-        data = measureUni(ranks, mesh_sizes, 'off', m)
-        ax[i].loglog(mesh_sizes, data["computeMapping"].values, "-d", label = "without")
-        
-        data = measureUni(ranks, mesh_sizes, 'tree', m)
-        ax[i].loglog(mesh_sizes, data["computeMapping"].values, "-d", label = "tree")
-                
-        
-        # ax[i].xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
-        ax[i].set_ylabel("Time [ms]")
-        ax[i].set_title('ranks = {}, m = {}'.format(ranks[0], m))
-
-    set_trace()
-
-    ax[0].legend()
-    ax[-1].set_xlabel("Mesh Size")
-    save_plot(filename)
-    plt.close()
-
-
-
-# measureScaling(lambda ranks: 200, 'strong-scaling-{prealloc}-mesh200')
-# measureScaling(lambda ranks: np.sqrt(ranks * 100), 'weak-scaling-{prealloc}-mesh100')
-# comparePreAllocVsNon(lambda ranks: 200, 'comparision')
-# problemUpscaling("problemupscaling")
 
 ppn = 24 # Processors per nodes, 24 for hazelhen, 28 for supermuc
-mpirun = "aprun" # for HazelHen
-# mpirun = "mpirun" # for SuperMUC and anywhere else
 
-nodes = 3
-ranksB = [(nodes-1)*ppn]
-ranksA = [ppn] * len(ranksB)
-mesh_sizes = [150] * len(ranksA)
+# mpirun = "aprun" # for HazelHen
+mpirun = "mpirun" # for SuperMUC and anywhere else
+
+# nodes = 3
+# ranksB = [(nodes-1)*ppn]
+# ranksA = [ppn] * len(ranksB)
+# mesh_sizes = [150] * len(ranksA)
 
 
-# preallocations = ["off", "compute", "saved", "tree"] * 2 # tree, saved, estimate, compute, off
+mesh_sizes = [50] * 4 + [60] * 4
+preallocations = ["off", "compute", "saved", "tree"] * 2 # tree, saved, estimate, compute, off
+ranksA = [2] * 8
+ranksB = [4] * 8
+ms = [6] * 8
 
+
+# preallocations = ["off", "compute", "saved", "tree"]
 preallocations = ["tree"] * len(ranksB)
 ms = [6] * len(ranksB)
 doScaling("prealloc", ranksA, ranksB, mesh_sizes, ms, preallocations)
