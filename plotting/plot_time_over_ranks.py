e4856e660edd1abec0ed34f3c232d16fa98c80eb
Florian Lindner <florian.lindner@ipvs.uni-stuttgart.de>
2018-03-21 18:00:10 +0100

    Make plotting labels configurable

--- plotting/plot_time_over_ranks.py
+++ plotting/plot_time_over_ranks.py
@@ -1,7 +1,15 @@
 import json, os, sys, pandas
 import matplotlib.pyplot as plt
+from ipdb import set_trace
 
 fields = ["PetRBF.fillA", "PetRBF.fillC", "PetRBF.preallocA", "PetRBF.preallocC"]
+
+labels = {"_GLOBAL" : "Total",
+          "PetRBF.fillA" : "Filling evaluation",
+          "PetRBF.fillC" : "Filling interpolation",
+          "PetRBF.preallocA" : "Preallocation evaluation",
+          "PetRBF.preallocC" : "Preallocation interpolation" }
+
 
 run_name = sys.argv[1] # like 2018-02-12T16:45:25.141337_testeins
 participant = "B"
@@ -12,13 +20,12 @@ info = json.load(open(run_name + ".meta"))
 
 df = pandas.read_csv(f_timings, comment = "#")
 
-
 for field in fields:
     ys = []
     for ts in df["Timestamp"].unique():
         ys.append( df[(df.Timestamp == ts) & (df.Name == field)].Avg.max() ) # Find the maximum time across all ranks
         
-    plt.plot(info["ranks" + participant], ys, label = field)
+    plt.plot(info["ranks" + participant], ys, label = labels[field])
     
 
 plt.grid()
