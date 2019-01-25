6e4b9fced89a560acefddc5247a4548345bea094
David Sommer <david97sommer@gmail.com>
2019-01-25 11:05:38 +0100

    Add linebreaks, improve reduce_dims, improve README, add visualize_mesh, fix AsteFlow, Fix typos

--- README.md
+++ README.md
@@ -9,22 +9,33 @@ aste consists of compiled C++ Modules and of Python modules.
 Reads a mesh as either `.vtk` or `.txt` and evaluates a function given by `-f` on it. The function gets the mesh points a numpy array of the form `x = [[x_1, y_1, z_1], ...]` and should store the result in `y`.
 ### partition_mesh.py
 Reads a mesh as either `.vtk` or `.txt`, partitions it and stores the parts in a directory like `dirname/0, 1, ...`. 
-This python module needs the C++ module `libmetisAPI.so`.
+For partitioning two algorithms are available. The meshfree algorithm does not need any mesh topology information, 
+whereas the topological algorithm needs topology information. 
+This python module needs the C++ module `libmetisAPI.so` if the topological algorithm is used.
 ### join_mesh.py
-Reads a partitioned mesh from a directory like `dirname/0, 1, ...` and saves it to a `.vtk` or `.txt` file.
+Reads a partitioned mesh from a directory like `dirname/0, 1, ...` and saves it to a single file.
+### libMetisAPI
+This is a small C++ wrapper around METIS. It is only required if `partition_mesh.py` should use a topological algorithm. 
+### preciceMap
+This is a C++ Module, which does the coupling through precice. It expects a mesh in the format given by `partition_mesh.py`.
 
 ## Dependencies
 ### C++ modules
+##### Required
 - preCICE
 - Eigen3
 - MPI
 - CMake
+##### Optional
 - METIS
 ### Python modules
+##### Required
 - NumPy
+##### Optional
 - vtk (Visualization Toolkit)
+- scipy
 ## Building
-Make sure to have all the dependencies installed. Then do:
+Make sure to have all the needed dependencies installed. Then do:
 ```
 mkdir build
 cd build
--- join_mesh.py
+++ join_mesh.py
@@ -14,7 +14,8 @@ def main():
 
 def read_mesh(dirname, length = None):
     """
-    Reads a mesh from the given directory. If length is given, only the first length parts are read, else length is inferred from the files. 
+    Reads a mesh from the given directory. If length is given, only the first length parts are read, 
+    else length is inferred from the files. 
     """
     if not length:
         length = 0
@@ -32,11 +33,15 @@ def read_mesh(dirname, length = None):
     return all_points, all_values
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Read a partitioned mesh and join it into a .vtk or .txt file.")
+    parser = argparse.ArgumentParser(description="""Read a partitioned mesh 
+            and join it into a .vtk or .txt file.""")
     parser.add_argument("in_meshname", metavar="inputmesh", help="The partitioned mesh used as input")
     parser.add_argument("--out", "-o", dest="out_meshname", help="The output mesh. Can be vtk or txt.")
-    parser.add_argument("--numparts", "-n", dest="numparts", type=int, help="The number of parts to read from the input mesh. By default the entire mesh is read.")
-    parser.add_argument("--log", "-l", dest="logging", default="INFO", choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], help="Set the log level. Default is INFO")
+    parser.add_argument("--numparts", "-n", dest="numparts", type=int, 
+            help="The number of parts to read from the input mesh. By default the entire mesh is read.")
+    parser.add_argument("--log", "-l", dest="logging", default="INFO", 
+            choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], help="Set the log level. Default is INFO")
     return parser.parse_args()
+
 if __name__ == "__main__":
     main()
