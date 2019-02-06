eeabf0fb6c04cfffd2e52466dd468e6667fa4b31
David Sommer <david97sommer@gmail.com>
2019-02-06 13:36:06 +0100

    Simplify eval_mesh.py, make vtk optional

--- src/eval_mesh.py
+++ src/eval_mesh.py
@@ -11,29 +11,28 @@ def main():
     points, cells, cell_types, _ = read_mesh(args.in_meshname)
     points = np.array(points)
     values = user_func(points, args.f_str)
-    out_meshname = args.out_meshname if args.out_meshname else args.in_meshname[:-4] + ".txt"
+    out_meshname = args.out_meshname if args.out_meshname else args.in_meshname
     write_mesh(out_meshname, points, cells, cell_types, values)
 
 def user_func(points, f_str = None):
+    import math
+    import numpy as np
     if not f_str:
         f_str = "y = np.zeros(len(x))"
-    x = np.array(points)
-    loc_dict = {"x": x}
-    exec(f_str, globals(), loc_dict)
-    y = loc_dict["y"]
-    if y is None:
-        raise Exception("Invalid function. y was not assigned a value")
-    if len(y) != len(x):
-        raise Exception("Invalid function. y has wrong shape.")
-    return y
+    points = np.array(points)
+    vals = np.zeros(points.shape[0])
+    for i, (x, y, z) in enumerate(points):
+        loc_dict = {"x": x, "y": y, "z": z, "math": math, "np": np}
+        vals[i] = eval(f_str, globals(), loc_dict)
+    return vals
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Evaluate a function on a given mesh")
     parser.add_argument("in_meshname", metavar="inputmesh", help="The mesh used as input")
     parser.add_argument("--func", "-f", dest="f_str", 
             help="""The function to evalutate on the mesh. 
-            Points are given in the form \"x = [[x1,y1,z1],[x2,y2,z2],...]\" as a numpy array. 
-            Output must be written to \"y\". Default is the 0 function.""")
+            Points are given in the form \"(x, y, z)\". An example for a function would be
+            \"math.sin(x) + math.exp(z)\". Default is the 0 function.""")
     parser.add_argument("--out", "-o", dest="out_meshname", help="""The output meshname. 
             Default is the same as for the input mesh""")
     parser.add_argument("--log", "-l", dest="logging", default="INFO", 
