d8699ca34d2e845a3705cd3647d401361c2c0822
richahert <richardhertrich@gmx.de>
2020-02-24 17:13:02 +0100

    consistent dimensions for data

--- src/precice_to_aste.py
+++ src/precice_to_aste.py
@@ -9,10 +9,17 @@ def run(cmd):
 # Generate Aste files from preCICE VTK output for every timestep
 def main():    
     args=parse_args()
-    files = [args.SolverName +".dt" + str(i) + ".vtk" for i in range(1,args.n)]
+    files = []
+    files.append(args.SolverName + ".init.vtk")
+    
+    for i in range(1,args.n+1):
+        files.append(args.SolverName +".dt" + str(i) + ".vtk")
     
     for i in files:
-        run("partition_mesh.py -n 1 " + i + " -t " + args.function)
+        run("partition_mesh.py -n 1 " + i + " -t " + args.function + " --dimf "+ str(args.dimf))
+    
+    run("rm -r " + args.SolverName + ".dt0")
+    run("mv " + args.SolverName + ".init " + args.SolverName + ".dt0")
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Convert preCICE VTK exports to ASTE format")
