541a85f08457d33801c55e91a56e745ee5bac998
Frédéric Simonis <simonisfrederic@gmail.com>
2020-05-08 14:51:06 +0200

    Add diff option to eval_mesh

--- src/eval_mesh.py
+++ src/eval_mesh.py
@@ -8,9 +8,15 @@ from mesh_io import read_mesh, write_mesh
 def main():
     args = parse_args()
     logging.basicConfig(level=getattr(logging, args.logging))
-    points, cells, cell_types, _ = read_mesh(args.in_meshname)
+    points, cells, cell_types, pointdata = read_mesh(args.in_meshname)
     points = np.array(points)
     values = user_func(points, args.function)
+    if args.diff:
+        values = np.array(pointdata) - values
+        logging.info("L2-norm of error {}".format(np.linalg.norm(values, 2)))
+        logging.info("Arithmetic mean of error {}".format(np.mean(values)))
+        logging.info("Variance of error {}".format(np.var(values)))
+
     write_mesh(args.out_meshname, points, cells, cell_types, values)
 
 
@@ -25,6 +31,7 @@ class Evaluator:
 
 
 def user_func(points, f_str):
+    import math
     points = np.array(points)
     evaluator = Evaluator(f_str)
     with Pool() as pool:
@@ -43,6 +50,7 @@ def parse_args():
             \"math.sin(x) + math.exp(z)\".""")
     parser.add_argument("--out", "-o", dest="out_meshname", help="""The output meshname. 
             Default is the same as for the input mesh""")
+    parser.add_argument("--diff", "-d", action='store_true', help="Calculate the difference to present data.")
     parser.add_argument("--log", "-l", dest="logging", default="INFO", 
             choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], help="""Set the log level. 
             Default is INFO""")
