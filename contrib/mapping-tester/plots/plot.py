83e266a7563d9d9cab1e11a8b90bcb9f9fdf36f6
Frédéric Simonis <simonisfrederic@gmail.com>
2020-10-16 10:33:59 +0200

    Improve plotting

--- contrib/mapping-tester/plots/plot.py
+++ contrib/mapping-tester/plots/plot.py
@@ -11,6 +11,8 @@ import itertools
 def parseArguments(args):
     parser = argparse.ArgumentParser(description="Creates convergence plots from gathered stats")
     parser.add_argument('-f', '--file', type=argparse.FileType('r'), default="stats.csv", help='The CSV file containing the gathered stats.')
+    parser.add_argument('-ni', '--no-inverse', dest="inverse", action="store_false", help='Do not create inverse plots.')
+    parser.add_argument('--show', action="store_true", help='Shows the plots insead of saving them.')
     return parser.parse_args(args)
 
 
@@ -29,8 +31,19 @@ def getStyler():
 def plot_order(ax, nth, xmin, xmax, ymin, ymax):
     x1, y1 = xmax, ymax
 
-    x2 = xmin
-    y2 = y1 * ((x2 / x1)**nth)
+    def f(x):
+        return y1 * ((x / x1)**nth)
+
+    xl, xu = xmin, xmax
+    for step in range(4):
+        xt = lavg([xu, xl])
+        yt = f(xt)
+
+        if yt > ymin:
+            xu = xt
+        if yt < ymin:
+            xl = xt
+    x2, y2 = xu, f(xu)
 
     xs, ys = [x1, x2], [y1, y2]
     ax.plot(xs, ys, color="lightgray", linewidth=1.0, zorder=-1)
@@ -50,15 +63,17 @@ def main(argv):
     df[numeric_cols] = df[numeric_cols].apply(pandas.to_numeric)
 
     # A on x axes
-    plot(df, False, True)
-    plot(df, False, False)
-    # B on x axes
-    plot(df, True, True)
-    plot(df, True, False)
+    plot(df, False, True, args.show)
+    plot(df, False, False, args.show)
+    
+    if (args.inverse):
+        # B on x axes
+        plot(df, True, True, args.show)
+        plot(df, True, False, args.show)
     return 0
 
 
-def plot(df, inverse, idfilter):
+def plot(df, inverse, idfilter, show=False):
     if inverse:
         xname = "mesh B"
         groupname = "mesh A"
@@ -70,14 +85,16 @@ def plot(df, inverse, idfilter):
     yname="relative-l2"
     styler = getStyler()
 
+    print("Plot x:{} y:{} grouped by {}".format(xname, yname, groupname))
+
     df.sort_values(xname, inplace=True)
     grouped = df.groupby(["mapping", groupname])
 
-    fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(10,8))
+    fig, ax = plt.subplots(sharex=True, sharey=True, figsize=(10,5))
 
     for name, group in grouped:
         filtered = group[group["mesh A"] != group["mesh B"]] if idfilter else group
-        verb = "from" if inverse else "onto"
+        print("\tGroup {} with {} points".format(fmt.format(*name), filtered.shape[0]))
         l, m, c = next(styler)
         filtered.plot(
             ax=ax,
@@ -92,24 +109,28 @@ def plot(df, inverse, idfilter):
     ax.set_xlabel("edge length(h) of {}".format(xname))
     ax.set_ylabel("{} error mapping to mesh B".format(yname))
 
-
-    filtered = df[df['mesh A'] != df['mesh B']][yname]
-    plot_order(ax, 1, df[xname].min(), df[xname].max(), filtered.min(), filtered.max())
-    plot_order(ax, 2, df[xname].min(), df[xname].max(), filtered.min(), filtered.max())
+    if(not inverse):
+        filtered = df[df['mesh A'] != df['mesh B']][yname]
+        plot_order(ax, 1, df[xname].min(), df[xname].max(), filtered.min(), filtered.max())
+        plot_order(ax, 2, df[xname].min(), df[xname].max(), filtered.min(), filtered.max())
+        plot_order(ax, 3, df[xname].min(), df[xname].max(), filtered.min(), filtered.max())
 
     plt.gca().invert_xaxis()
     plt.grid()
     ax.legend(loc="upper left", bbox_to_anchor=(1,1))
-    plt.subplots_adjust(right=0.75)
+    plt.subplots_adjust(right=0.7)
 
 
-    parts = ["plot"]
-    if inverse:
-        parts.extend("-inverse")
-    if idfilter:
-        parts.extend("-noid")
-    parts.extend(".pdf")
-    fig.savefig("".join(parts), pad_inches=1)
+    if show:
+        plt.show()
+    else:
+        parts = ["plot"]
+        if inverse:
+            parts.extend("-inverse")
+        if idfilter:
+            parts.extend("-noid")
+        parts.extend(".pdf")
+        fig.savefig("".join(parts), pad_inches=1)
 
 
 if __name__ == "__main__":
