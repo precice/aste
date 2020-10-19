bb5391d7b8d8f00115d79f67829ef7a0e46d61f0
Frédéric Simonis <simonisfrederic@gmail.com>
2020-10-19 14:28:15 +0200

    Add option to force regenerating meshes

--- contrib/mapping-tester/preparemeshes.py
+++ contrib/mapping-tester/preparemeshes.py
@@ -10,24 +10,41 @@ def parseArguments(args):
     parser = argparse.ArgumentParser(description="Prepares meshes for a test suite")
     parser.add_argument('-o', '--outdir', default="cases", help='Directory to generate the test suite in.')
     parser.add_argument('-s', '--setup', type=argparse.FileType('r'), default="setup.json", help='The test setup file to use.')
+    parser.add_argument('-f', '--force', action="store_true", help='Remove existing meshes.')
     return parser.parse_args(args)
 
 
-def prepareMainMesh(meshdir, name, file, function):
+def prepareMainMesh(meshdir, name, file, function, force=False):
     mainDir = os.path.join(meshdir, name, "1")
     mainMesh = os.path.join(mainDir, name+".txt")
     print("Preparing Mesh {} in {}".format(name, mainDir))
+
+    if os.path.isdir(mainDir):
+        if force:
+            print("  Regenerating the mesh.")
+            os.removedirs(mainDir)
+        else:
+            print("  Mesh already exists.")
+            return
 
     os.makedirs(mainDir, exist_ok=True)
     subprocess.run(["eval_mesh.py", file, "-o", mainMesh, function])
 
 
-def preparePartMesh(meshdir, name, p):
+def preparePartMesh(meshdir, name, p, force=False):
     assert(p > 1)
     mainMesh = os.path.join(meshdir, name, "1", name+".txt")
     partDir = os.path.join(meshdir, name, str(p))
     partMesh = os.path.join(partDir, name+".txt")
     print("Preparing Mesh {} with {} paritions in {}".format(name, p, partDir))
+
+    if os.path.isdir(partDir):
+        if force:
+            print("  Regenerating the partitioned mesh.")
+            os.removedirs(partDir)
+        else:
+            print("  Partitioned mesh already exists.")
+            return
 
     os.makedirs(partDir, exist_ok=True)
     subprocess.run(["partition_mesh.py", mainMesh, "-o", partMesh, "-n", str(p)])
@@ -46,9 +63,9 @@ def main(argv):
     partitions.discard(1)
 
     for name, file in set(itertools.chain(setup["general"]["meshes"]["A"].items(), setup["general"]["meshes"]["B"].items())):
-        prepareMainMesh(meshdir, name, file, function)
+        prepareMainMesh(meshdir, name, file, function, args.force)
         for p in partitions:
-            preparePartMesh(meshdir, name, p)
+            preparePartMesh(meshdir, name, p, args.force)
 
     return 0
 
