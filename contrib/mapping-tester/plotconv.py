77696a3ba8e78f06dd163dc0b589b89b90012ae4
Frédéric Simonis <simonisfrederic@gmail.com>
2021-07-27 09:33:15 +0200

    Minor updates

--- contrib/mapping-tester/plotconv.py
+++ contrib/mapping-tester/plotconv.py
@@ -14,34 +14,28 @@ def parseArguments(args):
 def lavg(l):
     return math.exp(sum(map(math.log, l)) / len(l))
 
-def main(argv):
-    args = parseArguments(argv[1:])
+# seaborn.color_palette("colorblind", 10).as_hex()
+style_colours = ['#0173b2', '#de8f05', '#029e73', '#d55e00', '#cc78bc', '#ca9161', '#fbafe4', '#949494', '#ece133', '#56b4e9']
+style_markers = ["o", "D", "s"]
+styles = [ (c, m) for m in style_markers for c in style_colours]
 
-    df = pandas.read_csv(args.file)
-    df.sort_values("mesh A", inplace=True)
-    grouped = df.groupby(["mapping", "mesh B"])
 
-    fig, ax = plt.subplots(sharex=True, sharey=True)
+def plotConv(ax, df, yname):
+    return
+    xmin = df["mesh A"].min()
+    xmax = df["mesh A"].max()
+    ymin = df[yname].min()
+    ymax = df[yname].max()
 
-    for name, group in grouped:
-        filtered = group # group[group["mesh A"] != group["mesh B"]]
-        filtered.plot(
-            ax=ax,
-            loglog=True,
-            x="mesh A",
-            y="relative-l2",
-            label="{} onto {}".format(*name),
-            marker="o"
-        )
-    ax.set_xlabel("edge length(h) of mesh A")
-    ax.set_ylabel("relative-l2 error mapping to mesh B")
+    print(xmin, xmax)
+    print(ymin, ymax)
 
     # 1st order line
-    fox = [0.0003, 0.1]
-    foy1 = 1e-6
+    fox = [xmax, xmin]
+    foy1 = ymax
     foy2 = foy1 * (fox[1]/fox[0])
     foy = [foy1, foy2]
-    ax.plot(fox, foy, color="lightgray", linewidth=1.0, zorder=-1)
+    ax.axline((fox[0], foy[0]), (fox[1], foy[1]), color="lightgray", linewidth=1.0, zorder=-1)
     ax.annotate(
         "1st order",
         xy=(lavg(fox), lavg(foy)),
@@ -50,14 +44,12 @@ def main(argv):
     )
 
     # # 2nd order line
-    sox = [0.0004, 0.01]
-    soy1 = 3e-9
+    sox = [xmin, xmax]
+    soy1 = ymin
     soy2 = soy1 * ((sox[1]/sox[0])**2)
     soy = [soy1, soy2]
-    ax.plot(sox, soy, color="lightgray", linewidth=1.0, zorder=-1)
-
-    print( sox, soy)
-    print( (sox[0] + sox[1])/2, (soy[0] + soy[1])/2)
+    print(sox, soy)
+    ax.axline((sox[0], soy[0]), (sox[1], soy[1]), color="lightgray", linewidth=1.0, zorder=-1)
     ax.annotate(
         "2nd order",
         xy=(lavg(sox), lavg(soy)),
@@ -65,10 +57,140 @@ def main(argv):
         zorder=-1
     )
 
+
+def plotError(df):
+    yname="relative-l2"
+    fig, ax = plt.subplots(sharex=True, sharey=True)
+    series = df.groupby("mapping")
+    for grouped, style in zip(series, styles):
+        name, group = grouped
+        if (group[yname].max() == 0):
+            print(f"Dropping {yname}-series {name} as all 0")
+            continue
+        color, marker = style
+        group.plot(
+            ax=ax,
+            loglog=True,
+            x="mesh A",
+            y=yname,
+            label=name,
+            marker=marker,
+            color=color
+        )
+    ax.set_xlabel("edge length(h) of mesh A")
+    ax.set_ylabel("relative-l2 error mapping to mesh B")
+
+    plotConv(ax, df, yname)
+
     plt.gca().invert_xaxis()
     plt.grid()
-    plt.show()
+    plt.savefig("turbine-small-error.pdf")
 
+
+def plotMemory(df):
+    yname="peakMemB"
+    fig, ax = plt.subplots(sharex=True, sharey=True)
+    series = df.groupby("mapping")
+    for grouped, style in zip(series, styles):
+        name, group = grouped
+        if (group[yname].max() == 0):
+            print(f"Dropping {yname}-series {name} as all 0")
+            continue
+        color, marker = style
+        group.plot(
+            ax=ax,
+            loglog=True,
+            x="mesh A",
+            y=yname,
+            label=name,
+            marker=marker,
+            color=color
+        )
+    ax.set_xlabel("edge length(h) of mesh A")
+    ax.set_ylabel("peak memory of participant B")
+
+    plotConv(ax, df, yname)
+
+    plt.gca().invert_xaxis()
+    plt.grid()
+    plt.savefig("turbine-small-peakMemB.pdf")
+
+
+def plotComputeMappingTime(df):
+    yname="computeMappingTime"
+    fig, ax = plt.subplots(sharex=True, sharey=True)
+    series = df.groupby("mapping")
+    for grouped, style in zip(series, styles):
+        name, group = grouped
+        if (group[yname].max() == 0):
+            print(f"Dropping {yname}-series {name} as all 0")
+            continue
+        color, marker = style
+        group.plot(
+            ax=ax,
+            loglog=True,
+            x="mesh A",
+            y=yname,
+            label=name,
+            marker=marker,
+            color=color
+        )
+
+    ax.set_xlabel("edge length(h) of mesh A")
+    ax.set_ylabel("time to compute mapping [ms]")
+
+    plotConv(ax, df, yname)
+
+    plt.gca().invert_xaxis()
+    plt.grid()
+    plt.savefig("turbine-small-computet.pdf")
+
+
+def plotMapDataTime(df):
+    yname="mapDataTime"
+    fig, ax = plt.subplots(sharex=True, sharey=True)
+    series = df.groupby("mapping")
+    for grouped, style in zip(series, styles):
+        name, group = grouped
+        if (group[yname].max() == 0):
+            print(f"Dropping {yname}-series {name} as all 0")
+            continue
+        color, marker = style
+        group.plot(
+            ax=ax,
+            loglog=True,
+            x="mesh A",
+            y=yname,
+            label=name,
+            marker=marker,
+            color=color
+        )
+
+    ax.set_xlabel("edge length(h) of mesh A")
+    ax.set_ylabel("time to map Data [ms]")
+
+    plotConv(ax, df, yname)
+
+    plt.gca().invert_xaxis()
+    plt.grid()
+    plt.savefig("turbine-small-mapt.pdf")
+
+
+def main(argv):
+    args = parseArguments(argv[1:])
+
+    plt.rcParams["legend.fontsize"] = "small"
+    plt.rcParams["figure.figsize"] = "8, 8"
+    plt.rcParams["figure.autolayout"] = "true"
+
+    df = pandas.read_csv(args.file)
+    toMeshes = df["mesh B"].unique()
+    assert len(toMeshes) == 1, f"There are {len(toMeshes)} to-meshes but only 1 is allowed. Fix your dataset!"
+    df.sort_values("mesh A", inplace=True)
+    plotError(df)
+    plotMemory(df)
+    plotMapDataTime(df)
+    plotComputeMappingTime(df)
     return 0
 
 
