c914346049b1a2e0b9ad88cb149cb2c5907feedc
Frédéric Simonis <simonisfrederic@gmail.com>
2021-08-30 16:24:03 +0200

    Setup demo

--- contrib/reinit-demo/demo.sh
+++ contrib/reinit-demo/demo.sh
@@ -5,17 +5,22 @@ set -x
 
 PATH=$(pwd)/../../build:$PATH
 
+make -j $(nproc) -C $(pwd)/../../build
+
 APARTS=2
 BPARTS=4
 
 # Prepare A meshes for various timesteps
 for t in 0 1 2 3; do
-  eval_mesh.py 0.004.vtk -o "0.004.d$t.vtk" "$t"
-  partition_mesh.py "0.004.d$t.vtk" -n $APARTS
+  eval_mesh.py 0.004.vtk -o "0.004.dt$t.vtk" "$t"
+  partition_mesh.py "0.004.dt$t.vtk" -n $APARTS
 done
 
 partition_mesh.py 0.009.vtk -n $BPARTS
 
-### Map from the bunny to the red blood cell (yeah, that doesn't really make sense)
-mpirun -n $APARTS preciceMap -v -p A --mesh 0.004 &
-mpirun -n $BPARTS preciceMap -v -p B --mesh 0.009 --output mapped
+set -m
+(
+mpirun -n $APARTS preciceMap -v -p A --mesh "0.004" &
+mpirun -n $BPARTS preciceMap -v -p B --mesh "0.009" --output mapped &
+wait
+)
