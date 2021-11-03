f3580855a5375bf0a6b833df26237c12234f4eab
Kursat_Yurt <kursatyurt@gmail.com>
2021-11-03 19:02:58 +0100

    Add input datatag

--- src/vtk_calculator.py
+++ src/vtk_calculator.py
@@ -17,8 +17,10 @@ def parse_args():
             Syntax is the same as used in the calculator object, coordinates are given as e.g. 'coordsX+coordsY'.""")
     parser.add_argument("--out", "-o", dest="out_meshname", default=None, help="""The output meshname.
             Default is the same as for the input mesh""")
-    parser.add_argument("--tag", "-t", dest="tag", default="MyScalar", help="""The tag for data.
+    parser.add_argument("--tag", "-t", dest="tag", default="MyScalar", help="""The tag for output data.
             Default is MyScalar""")
+    parser.add_argument("--intag", "-it", dest="intag", help="""The tag for input data.
+            Required for diff mod""")      
     parser.add_argument("--log", "-l", dest="logging", default="INFO",
                         choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], help="""Set the log level.
             Default is INFO""")
@@ -46,37 +48,23 @@ def main():
     vtk_dataset = reader.GetOutput()
     logging.info("Read in {} points.".format(vtk_dataset.GetNumberOfPoints()))
 
-    if not args.diff:
-        calc = vtk.vtkArrayCalculator()
-        calc.SetInputData(vtk_dataset)
-        calc.AddCoordinateScalarVariable("coordsX", 0)
-        calc.AddCoordinateScalarVariable("coordsY", 1)
-        calc.AddCoordinateScalarVariable("coordsZ", 2)
-        calc.SetFunction(args.function)
-        calc.SetResultArrayName(args.tag)
-        calc.Update()
-
-        logging.info("Evaluated '{}' on the mesh.".format(args.function))
-        writer = vtk.vtkUnstructuredGridWriter()
-        writer.SetInputData(calc.GetOutput())
-        writer.SetFileName(out_meshname)
-        writer.Write()
-        logging.info("Written output to {}.".format(out_meshname))
-
+    calc = vtk.vtkArrayCalculator()
+    calc.SetInputData(vtk_dataset)
+    calc.AddCoordinateScalarVariable("coordsX", 0)
+    calc.AddCoordinateScalarVariable("coordsY", 1)
+    calc.AddCoordinateScalarVariable("coordsZ", 2)
+    calc.SetFunction(args.function)
+    calc.SetResultArrayName(args.tag)
+    calc.Update()    
+    logging.info("Evaluated '{}' on the mesh.".format(args.function))
+    
     if args.diff:
-        diff_calc = vtk.vtkArrayCalculator()
-        diff_calc.SetInputData(vtk_dataset)
-        diff_calc.AddCoordinateScalarVariable("coordsX", 0)
-        diff_calc.AddCoordinateScalarVariable("coordsY", 1)
-        diff_calc.AddCoordinateScalarVariable("coordsZ", 2)
-        diff_calc.SetFunction(args.function)
-        diff_calc.SetResultArrayName("Calculated Value")
-        diff_calc.Update()
+        assert args.intag is not None, "Difference mode needs input array tag"
 
         org_val = []
         calc_val = []
-        orgArr = diff_calc.GetOutput().GetPointData().GetAbstractArray(args.tag)
-        calcArr = diff_calc.GetOutput().GetPointData().GetAbstractArray("Calculated Value")
+        orgArr = calc.GetOutput().GetPointData().GetAbstractArray(args.intag)
+        calcArr = calc.GetOutput().GetPointData().GetAbstractArray(args.tag)
         num_points = vtk_dataset.GetNumberOfPoints()
         for i in range(num_points):
             org_val.append(orgArr.GetTuple1(i))
@@ -108,6 +96,11 @@ def main():
                 "90th percentile": p90
             }, open(base + ".stats.json", "w"))
 
+    writer = vtk.vtkUnstructuredGridWriter()
+    writer.SetInputData(calc.GetOutput())
+    writer.SetFileName(out_meshname)
+    writer.Write()
+    logging.info("Written output to {}.".format(out_meshname))
 
 if __name__ == "__main__":
     main()
