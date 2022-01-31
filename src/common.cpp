8a0afeb09d58142682309b82bd24ea870d4a0033
Kursat Yurt <kursatyurt@gmail.com>
2022-01-31 19:46:04 +0000

    Remove not required parsing values

--- src/common.cpp
+++ src/common.cpp
@@ -14,23 +14,15 @@ OptionMap getOptions(int argc, char *argv[])
   desc.add_options()("help,h", "Print this help message")(
       "precice-config,c",
       po::value<std::string>()->default_value("precice-config.xml"),
-      "preCICE configuratio file")(
-      "participant,p",
-      po::value<std::string>()->required(),
-      "Participant Name")(
-      "data", po::value<std::string>()->required(),
-      "Name of Data Array to be Mapped")(
-      "mesh", po::value<string>()->required(),
-      "Mesh prefix (i.e. mesh name without the format extension such as '.vtk' or '.vtu'). "
-      "Example: solution.vtk has the prefix 'solution'. "
-      "preciceMap will look for timeseries as well as distributed meshes (e.g. from preCICE exports) "
-      "automatically and load them if required.")(
-      "output", po::value<string>()->default_value("output"),
-      "Output file name.")(
-      "vector", po::bool_switch(),
-      "Distinguish between vector valued data and scalar data")(
-      "verbose,v", po::bool_switch(),
-      "Enable verbose output"); // not explicitely used, handled by easylogging
+      "preCICE configuration file") ("participant,p",
+                                     po::value<std::string>()->required(),
+                                     "Participant Name") ("mesh", po::value<string>()->required(),
+                                                          "Mesh prefix (i.e. mesh name without the format extension such as '.vtk' or '.vtu'). "
+                                                          "Example: solution.vtk has the prefix 'solution'. "
+                                                          "preciceMap will look for timeseries as well as distributed meshes (e.g. from preCICE exports) "
+                                                          "automatically and load them if required.") ("output,o", po::value<string>()->default_value("output"),
+                                                                                                       "Output file name.") ("verbose,v", po::bool_switch(),
+                                                                                                                             "Enable verbose output"); // not explicitely used, handled by easylogging
 
   po::variables_map vm;
 
@@ -44,10 +36,6 @@ OptionMap getOptions(int argc, char *argv[])
 
     // Needs to be called before we look for participants
     po::notify(vm);
-
-    if (vm["participant"].as<string>() != "A" &&
-        vm["participant"].as<string>() != "B")
-      throw runtime_error("Invalid participant, must be either 'A' or 'B'");
   } catch (const std::exception &e) {
     LOG(ERROR) << "ERROR: " << e.what() << "\n";
     LOG(ERROR) << desc << std::endl;
