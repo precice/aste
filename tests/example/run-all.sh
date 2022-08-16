b105c0944fe8b123593c216eeb92ece551ef1344
Kursat Yurt <kursatyurt@gmail.com>
2022-08-16 11:19:21 +0200

    Run all cases and exit

--- tests/example/run-all.sh
+++ tests/example/run-all.sh
@@ -1,21 +1,33 @@
 #!/bin/sh
 set -e -u
 
-error_exit(){
+# Print the problmatic case and increase error count
+detect_error(){
     echo "Error while running case ${array[1]}"
-    exit 1
+    err_count=$((err_count + 1))
 }
 
-set -e -u
-
 echo "- Running all examples cases..."
-testcases=$(find . -maxdepth 2 -mindepth 2 -name run.sh)
-numberofcases=$(echo $testcases | wc -w)
-echo "- There are $numberofcases case(s) found..."
+test_cases=$(find . -maxdepth 2 -mindepth 2 -name run.sh)
+number_of_cases=$(echo $test_cases | wc -w)
+echo "- There are $number_of_cases case(s) found..."
 
-for testcase in $testcases; do
-    IFS='/' read -ra array <<< "$testcase"
+# Initial directory and error count
+run_all_dir=$(pwd)
+err_count=0
+
+# Run all cases
+for testcase in $test_cases; do
+    array=($(echo $testcase | tr "/" "\n"))
     cd ${array[1]}
     echo "- Running test case ${array[1]}..."
-    ./run.sh || error_exit && cd -
+    ./run.sh || detect_error
+    cd $run_all_dir
 done
+
+if [  $err_count -eq 0 ]; then
+    echo "All test are completed succesfully"
+else
+    echo "$err_count out of $number_of_cases cannot be completed"
+    exit 1
+fi
