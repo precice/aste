84e3dd5ca099767fde932facb947e033eff34e0b
Kürşat Yurt <57598663+kursatyurt@users.noreply.github.com>
2022-08-16 15:18:36 +0200

    Exit on error in run all script (#132)
    * Exit on error in run all script
    
    * Remove redirection
    
    * Run all cases and exit
    
    * Fix syntax; Move sh to bash
    
    * Add missing parantesis
    
    * Update tests/example/run-all.sh
    
    Co-authored-by: David Schneider <david.schneider@ipvs.uni-stuttgart.de>
    
    * Update tests/example/run-all.sh
    
    Co-authored-by: David Schneider <david.schneider@ipvs.uni-stuttgart.de>
    
    Co-authored-by: David Schneider <david.schneider@ipvs.uni-stuttgart.de>

--- tests/example/run-all.sh
+++ tests/example/run-all.sh
@@ -1,6 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 set -e -u
 
-echo "- Running all examples cases..."
+# Print the problmatic case and increase error count
+detect_error(){
+    echo "Error while running case ${array[1]}"
+    err_count=$((err_count + 1))
+}
 
-find . -maxdepth 2 -mindepth 2 -name run.sh -execdir sh -c './run.sh' \;
+echo "- Running all examples cases..."
+test_cases=$(find . -maxdepth 2 -mindepth 2 -name run.sh)
+number_of_cases=$(echo "$test_cases" | wc -w)
+echo "- There are $number_of_cases case(s) found..."
+
+# Initial directory and error count
+run_all_dir=$(pwd)
+err_count=0
+
+# Run all cases
+for testcase in $test_cases; do
+    array=($(echo "$testcase" | tr "/" "\n"))
+    cd "${array[1]}"
+    echo "- Running test case ${array[1]}..."
+    ./run.sh || detect_error
+    cd "$run_all_dir"
+done
+
+if [  $err_count -eq 0 ]; then
+    echo "All tests passed"
+else
+    echo "$err_count out of $number_of_cases failed"
+    exit 1
+fi
