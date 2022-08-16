45c9c83a8d463ac062a7a1ebef9c919d7de7078d
Kursat Yurt <kursatyurt@gmail.com>
2022-08-16 11:29:38 +0200

    Fix syntax; Move sh to bash

--- tests/example/run-all.sh
+++ tests/example/run-all.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -e -u
 
 # Print the problmatic case and increase error count
@@ -9,7 +9,7 @@ detect_error(){
 
 echo "- Running all examples cases..."
 test_cases=$(find . -maxdepth 2 -mindepth 2 -name run.sh)
-number_of_cases=$(echo $test_cases | wc -w)
+number_of_cases=$(echo "$test_cases" | wc -w)
 echo "- There are $number_of_cases case(s) found..."
 
 # Initial directory and error count
@@ -18,11 +18,11 @@ err_count=0
 
 # Run all cases
 for testcase in $test_cases; do
-    array=($(echo $testcase | tr "/" "\n"))
-    cd ${array[1]}
+    array=$(echo "$testcase" | tr "/" "\n")
+    cd "${array[1]}"
     echo "- Running test case ${array[1]}..."
     ./run.sh || detect_error
-    cd $run_all_dir
+    cd "$run_all_dir"
 done
 
 if [  $err_count -eq 0 ]; then
